c17b2480132bfeaa5c0a8c2a3e4ab70cc6c2ae06
Samuel <samlexrod@outlook.com>
2024-11-29 16:29:56 +0000

    Added bytearray support to pre-processing

--- inference/inference.py
+++ inference/inference.py
@@ -2,6 +2,7 @@ import io
 import torch
 import torchvision.transforms as transforms
 from PIL import Image
+import base64
 
 # Define the transformation pipeline
 transform = transforms.Compose([
@@ -11,26 +12,42 @@ transform = transforms.Compose([
 ])
 
 def preprocess_input(request_body, content_type="application/x-image"):
-    if content_type in ["application/x-image", "text/csv"]:
-        # Load the image from bytes
-        image = Image.open(io.BytesIO(request_body)).convert("RGB")
+    print("*"*60)
+    print(f"Pre-processing request_body {request_body}")
+    print("*"*60)
+
+    try:
+        # Decode base64 if the request body is a string
+        if isinstance(request_body, str):
+            request_body = base64.b64decode(request_body)
+            print(f"Pre-processing converted request_body {request_body}")
         
-        # Apply the transformations
-        tensor = transform(image).unsqueeze(0)  # Add batch dimension
-        return tensor
-    else:
-        raise ValueError(f"Unsupported content type: {content_type}")
+        if content_type in ["application/x-image", "text/csv"]:
+            # Load the image from bytes
+            image = Image.open(io.BytesIO(request_body)).convert("RGB")
+            
+            # Apply the transformations
+            tensor = transform(image).unsqueeze(0)  # Add batch dimension
+            return tensor
+        else:
+            raise ValueError(f"Unsupported content type: {content_type}")
+    except Exception as e:
+        raise Exception(f"Error in preprocess_input {e}")
+        
 
 def input_fn(request_body, content_type):
-    
     print("*"*60)
+    print(f"-> Input content type {content_type}") 
     print("*"*60)
-    print(f"-> Pre-processing content type {content_type}") 
     
     # Preprocess the input using the logic defined
     return preprocess_input(request_body, content_type)
 
 def predict_fn(input_data, model):
+    print("*"*60)
+    print(f"Predicting {input_data}")
+    print("*"*60)
+    
     # Perform inference using the preprocessed data
     with torch.no_grad():
         outputs = model(input_data)
